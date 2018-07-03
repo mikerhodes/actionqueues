cf0a9dbd407b2bbafa1dd742891843dc239baf91
Mike Rhodes <mike.rhodes@gmail.com>
2018-07-03 20:40:38 +0100

    Initial import

---
+++ actionstacks/README.md
@@ -0,0 +1,4 @@
+# Action Stacks
+
+`actionstacks` is a lightweight way to queue up commands for execution and
+rollback on failures.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='actionstacks',
+      version='0.1',
+      description='Framework for executing actions and rollbacks',
+      url='http://github.com/mikerhodes/actionstacks',
+      author='Mike Rhodes',
+      author_email='mike.rhodes@dx13.co.uk',
+      license='Apache 2.0',
+      packages=['actionstacks'],
+      zip_safe=False)
