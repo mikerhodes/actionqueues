45feb3dfa8fd741de6eb11fc2e63afc194b3cc93
Mike Rhodes <mike.rhodes@gmail.com>
2018-07-03 21:26:41 +0100

    Show raising from execute allows correct rollback
    It turns out that just throwing any exception from the execute() method
    will allow rollback to happen from the right place, so add a test to
    capture this state.

--- actionqueues/tests/test_actionqueues.py
+++ actionqueues/tests/test_actionqueues.py
@@ -19,6 +19,19 @@ class MockCommand(action.Action):
         self._rollback_called = True
         self._rollback_value = self._rollback_state.inc()
 
+class ExplodingCommand(action.Action):
+
+    def __init__(self):
+        self._execute_called = False
+        self._rollback_called = False
+
+    def execute(self):
+        self._execute_called = True
+        raise IOError()
+
+    def rollback(self):
+        self._rollback_called = True
+
 class State(object):
     """The state object maintains a simple counter that allows
     us to check Actions are executed in order."""
@@ -64,6 +77,36 @@ def test_rollback_actions():
     for idx,action in enumerate(reversed(actions)):
         assert action._rollback_called
         assert action._rollback_value == idx+1
+
+def test_explode_action():
+    exec_state = State()
+    rollback_state = State()
+    actions = [
+        MockCommand(exec_state, rollback_state),
+        MockCommand(exec_state, rollback_state),
+        ExplodingCommand(),
+        MockCommand(exec_state, rollback_state)
+    ]
+    q = actionqueue.ActionQueue()
+    for action in actions:
+        q.add(action)
+
+    try:
+        q.execute()
+    except:
+        pass  # expected due to ExplodingCommand
+
+    assert actions[0]._execute_called
+    assert actions[1]._execute_called
+    assert actions[2]._execute_called
+    assert not actions[3]._execute_called
+
+    q.rollback()
+
+    assert actions[0]._rollback_called
+    assert actions[1]._rollback_called
+    assert actions[2]._rollback_called
+    assert not actions[3]._rollback_called
 
 
 
